91b49fe1946b5afbbbf39a2c93c14d0658f29b54
bartekparysek <bartek.parysek@gmail.com>
2022-01-18 21:36:45 +0100

    changed files to .tsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import LoanParams from './Components/LoanParams';
+import ClientInfo from './Components/ClientInfo';
+import Invitation from './Components/Invitation';
+import Finish from './Components/Finish';
+import Header from './Components/Header';
+import './styles.css';
+
+export default function App() {
+	return (
+		<Router>
+			<div className='app'>
+				<div className='section'>
+					<Header />
+					<Switch>
+						<Route path='/' exact component={Invitation} />
+						<Route path='/loanparams' component={LoanParams} />
+						<Route path='/clientinfo' component={ClientInfo} />
+						<Route path='/finish' component={Finish} />
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
+}
---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (): JSX.Element => {
+	return (
+		<div>
+			<h1>Cash Loan App</h1>
+		</div>
+	);
+};
+export default Header;
