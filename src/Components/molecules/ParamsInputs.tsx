2531e0bdfe546ae29fe239378319f5e0022ff496
bartekparysek <bartek.parysek@gmail.com>
2022-01-26 16:50:38 +0100

    added Parameter Inputs parent component

---
+++ src/Components/molecules/ParamsInputs.tsx
@@ -0,0 +1,7 @@
+const ParamsInputs = ({
+	children,
+}: JSX.ElementChildrenAttribute): JSX.Element => {
+	return <div className='flex items-end mb-2 '>{children}</div>;
+};
+
+export default ParamsInputs;
